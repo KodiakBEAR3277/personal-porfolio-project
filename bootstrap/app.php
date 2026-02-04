b6e31b6b55bc79964af2561224f4ecf3ffc81c15
KodiakBEAR3277 <chrisgeosan404@gmail.com>
2026-02-04 16:50:25 +0800

    Config Changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+    $app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+
+return $app;
